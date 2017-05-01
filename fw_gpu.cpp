df3fd121782064019183e476c8e480a14b456b0b
Swaminathan Sivaraman <sswaminathan92@gmail.com>
2017-05-01 13:12:31 -0400

    Using 64-bit unsigned integers everywhere for safety

--- fw_gpu.cpp
+++ fw_gpu.cpp
@@ -33,40 +33,42 @@ typedef stxxl::VECTOR_GENERATOR<unsigned long, BLOCKS_PER_PAGE, PAGES_IN_CACHE,
  * Encoding and decoding Morton codes
  * (Taken from https://fgiesen.wordpress.com/2009/12/13/decoding-morton-codes/)
  */
-uint32_t encode2D_to_morton_32bit(uint32_t x, uint32_t y)
+uint64_t encode2D_to_morton_64bit(uint64_t x, uint64_t y)
 {
-    x &= 0x0000ffff;
-    x = (x ^ (x <<  8)) & 0x00ff00ff;
-    x = (x ^ (x <<  4)) & 0x0f0f0f0f;
-    x = (x ^ (x <<  2)) & 0x33333333;
-    x = (x ^ (x <<  1)) & 0x55555555;
+    x &= 0x00000000ffffffff;
+    x = (x ^ (x <<  16)) & 0x0000ffff0000ffff;
+    x = (x ^ (x <<  8))  & 0x00ff00ff00ff00ff;
+    x = (x ^ (x <<  4))  & 0x0f0f0f0f0f0f0f0f;
+    x = (x ^ (x <<  2))  & 0x3333333333333333;
+    x = (x ^ (x <<  1))  & 0x5555555555555555;
 
-    y &= 0x0000ffff;
-    y = (y ^ (y <<  8)) & 0x00ff00ff;
-    y = (y ^ (y <<  4)) & 0x0f0f0f0f;
-    y = (y ^ (y <<  2)) & 0x33333333;
-    y = (y ^ (y <<  1)) & 0x55555555;
+    y &= 0x00000000ffffffff;
+    y = (y ^ (y <<  16)) & 0x0000ffff0000ffff;
+    y = (y ^ (y <<  8))  & 0x00ff00ff00ff00ff;
+    y = (y ^ (y <<  4))  & 0x0f0f0f0f0f0f0f0f;
+    y = (y ^ (y <<  2))  & 0x3333333333333333;
+    y = (y ^ (y <<  1))  & 0x5555555555555555;
 
-    return (x << 1 ) | y;
-
+    // This will return row-major order z ordering. If we switch x and y, it will be column-major
+    return (x << 1) | y;
 }
 
 /**
  * Serial Floyd-Warshall's algorithm
  */
 void serial_fw(unsigned long *X, unsigned long *U, unsigned long *V,
-               int xrow, int xcol, int urow, int ucol, int vrow, int vcol,
-               int n)
+               uint64_t xrow, uint64_t xcol, uint64_t urow, uint64_t ucol, uint64_t vrow, uint64_t vcol,
+               uint64_t n)
 {
-    for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int Xi = xrow + i; int Xj = xcol + j;
-                int Ui = urow + i; int Vj = vcol + j;
-                int K  = ucol + k;
-                int cur = encode2D_to_morton_32bit(Xi, Xj);
-                int first = encode2D_to_morton_32bit(Ui, K);
-                int second = encode2D_to_morton_32bit(K, Vj);
+    for (uint64_t k = 0; k < n; k++) {
+        for (uint64_t i = 0; i < n; i++) {
+            for (uint64_t j = 0; j < n; j++) {
+                uint64_t Xi = xrow + i; uint64_t Xj = xcol + j;
+                uint64_t Ui = urow + i; uint64_t Vj = vcol + j;
+                uint64_t K  = ucol + k;
+                uint64_t cur = encode2D_to_morton_64bit(Xi, Xj);
+                uint64_t first = encode2D_to_morton_64bit(Ui, K);
+                uint64_t second = encode2D_to_morton_64bit(K, Vj);
                 X[cur] = min(X[cur], (U[first] + V[second]));
             }
         }
@@ -77,25 +79,25 @@ void serial_fw(unsigned long *X, unsigned long *U, unsigned long *V,
  * RAM code
  */
 void host_RAM_A_fw(unsigned long *X,
-                   int xrow, int xcol, int n)
+                   uint64_t xrow, uint64_t xcol, uint64_t n)
 {
     serial_fw(X, X, X, xrow, xcol, xrow, xcol, xrow, xcol, n);
 }
 
 void host_RAM_B_fw(unsigned long *X, unsigned long *U,
-                   int xrow, int xcol, int urow, int ucol, int n)
+                   uint64_t xrow, uint64_t xcol, uint64_t urow, uint64_t ucol, uint64_t n)
 {
     serial_fw(X, U, X, xrow, xcol, urow, ucol, xrow, xcol, n);
 }
 
 void host_RAM_C_fw(unsigned long *X, unsigned long *V,
-                   int xrow, int xcol, int vrow, int vcol, int n)
+                   uint64_t xrow, uint64_t xcol, uint64_t vrow, uint64_t vcol, uint64_t n)
 {
     serial_fw(X, X, V, xrow, xcol, xrow, xcol, vrow, vcol, n);
 }
 
 void host_RAM_D_fw(unsigned long *X, unsigned long *U, unsigned long *V,
-                   int xrow, int xcol, int urow, int ucol, int vrow, int vcol, int n)
+                   uint64_t xrow, uint64_t xcol, uint64_t urow, uint64_t ucol, uint64_t vrow, uint64_t vcol, uint64_t n)
 {
     serial_fw(X, U, V, xrow, xcol, urow, ucol, vrow, vcol, n);
 }
@@ -104,12 +106,12 @@ void host_RAM_D_fw(unsigned long *X, unsigned long *U, unsigned long *V,
 /**
  * Read from stxxl vector to RAM vector
  */
-void read_to_RAM(fw_vector_type& zfloyd, unsigned long *X, int row, int col, int n)
+void read_to_RAM(fw_vector_type& zfloyd, unsigned long *X, uint64_t row, uint64_t col, uint64_t n)
 {
     cout << "Reading from row " << row << " and col " << col << " of size " << n << " from disk" << endl; 
-    int i = 0;
-    int begin_pos = encode2D_to_morton_32bit(row, col);
-    int end_pos = begin_pos + (n*n) - 1;
+    uint64_t i = 0;
+    uint64_t begin_pos = encode2D_to_morton_64bit(row, col);
+    uint64_t end_pos = begin_pos + (n*n) - 1;
     for (fw_vector_type::const_iterator it = zfloyd.begin() + begin_pos; it != zfloyd.begin() + end_pos + 1; ++it, ++i) {
         X[i] = *it;
     }
@@ -118,12 +120,12 @@ void read_to_RAM(fw_vector_type& zfloyd, unsigned long *X, int row, int col, int
 /**
  * Write RAM data to stxxl vector
  */
-void write_to_disk(fw_vector_type& zfloyd, unsigned long *X, int row, int col, int n)
+void write_to_disk(fw_vector_type& zfloyd, unsigned long *X, uint64_t row, uint64_t col, uint64_t n)
 {   
     cout << "Writing from row " << row << " and col " << col << " of size " << n << " to disk" << endl; 
-    int i = 0;
-    int begin_pos = encode2D_to_morton_32bit(row, col);
-    int end_pos = begin_pos + (n*n) - 1;
+    uint64_t i = 0;
+    uint64_t begin_pos = encode2D_to_morton_64bit(row, col);
+    uint64_t end_pos = begin_pos + (n*n) - 1;
     for (fw_vector_type::iterator it = zfloyd.begin() + begin_pos; it != zfloyd.begin() + end_pos + 1; ++it, ++i) {
         *it = X[i];
     }
@@ -133,7 +135,7 @@ void write_to_disk(fw_vector_type& zfloyd, unsigned long *X, int row, int col, i
  * Disk code
  */
 void host_disk_A_fw(fw_vector_type& zfloyd,
-                    int xrow, int xcol, int n)
+                    uint64_t xrow, uint64_t xcol, uint64_t n)
 {
     // Base case - If possible, read the entire array into RAM
     if (n <= ALLOWED_SIZE_RAM) {
@@ -145,8 +147,8 @@ void host_disk_A_fw(fw_vector_type& zfloyd,
     }
     // If not, split into r chunks
     else {
-        int r = n / ALLOWED_SIZE_RAM;
-        int m = n / r;
+        uint64_t r = n / ALLOWED_SIZE_RAM;
+        uint64_t m = n / r;
         cout << "Splitting disk matrix into r=" << r << " chunks, each submatrix size, m=" << m << endl;
 
         // Our RAM size is chosen such that it holds upto 3 times the allowed size
@@ -154,7 +156,7 @@ void host_disk_A_fw(fw_vector_type& zfloyd,
         unsigned long *R1 = new unsigned long[m*m];
         unsigned long *R2 = new unsigned long[m*m];
 
-        for (int k = 0; k < r; k++) {
+        for (uint64_t k = 0; k < r; k++) {
             cout << "k " << k << endl;
 
             // Step 1: A_step - A(X_kk, U_kk, V_kk), X,U,V are the same
@@ -168,18 +170,18 @@ void host_disk_A_fw(fw_vector_type& zfloyd,
             // For B, X and V are the same, for C, X and U are the same
             cout << "B\n";
             read_to_RAM(zfloyd, R1, xrow + (m*k), xcol + m*k, m);
-            for (int j = 0; j < r; j++) {
+            for (uint64_t j = 0; j < r; j++) {
                 if (j != k) {
-                    cout << "Bj " << j << endl;
+                    // DEBUG: cout << "Bj " << j << endl;
                     read_to_RAM(zfloyd, W, xrow + (m*k), xcol + m*j, m);
                     host_RAM_B_fw(W, R1, 0, 0, 0, 0, m);
                     write_to_disk(zfloyd, W, xrow + (m*k), xcol + m*j, m);
                 }
             }
             cout << "C\n";
-            for (int i = 0; i < r; i++) {
+            for (uint64_t i = 0; i < r; i++) {
                 if (i != k) {
-                    cout << "Ci " << i << endl;
+                    // DEBUG: cout << "Ci " << i << endl;
                     read_to_RAM(zfloyd, W, xrow + (m*i), xcol + m*k, m);
                     host_RAM_C_fw(W, R1, 0, 0, 0, 0, m);
                     write_to_disk(zfloyd, W, xrow + (m*i), xcol + m*k, m);
@@ -188,13 +190,13 @@ void host_disk_A_fw(fw_vector_type& zfloyd,
 
             // Step 3: D_step - D(X_ij, U_ik, V_kj)
             cout << "D\n";
-            for (int i = 0; i < r; i++) {
+            for (uint64_t i = 0; i < r; i++) {
                 if (i != k) {
                     // U_ik is same for all j
                     read_to_RAM(zfloyd, R1, xrow + (m*i), xcol + m*k, m);
-                    for (int j = 0; j < r; j++) {
+                    for (uint64_t j = 0; j < r; j++) {
                         if (j != k) {
-                            cout << "Di " << i << ", Dj " << j << endl; 
+                            // DEBUG: cout << "Di " << i << ", Dj " << j << endl; 
                             read_to_RAM(zfloyd, R2, xrow + (m*k), xcol + m*j, m);
                             read_to_RAM(zfloyd, W, xrow + (m*i), xcol + m*j, m);
                             host_RAM_D_fw(W, R1, R2, 0, 0, 0, 0, 0, 0, m);
@@ -226,7 +228,7 @@ int main(int argc, char *argv[])
     ifstream inp_file(inp_filename);
      
     string line;
-    int full_size = 0;
+    uint64_t full_size = 0;
     unsigned long value = 0;
     cout << "Reading input file, " << inp_filename << endl;
     while (getline(inp_file, line)) {
@@ -250,7 +252,7 @@ int main(int argc, char *argv[])
         cout << *it << " ";
     cout << endl;
 
-    int n = sqrt(full_size);
+    uint64_t n = sqrt(full_size);
     host_disk_A_fw(zfloyd, 0, 0, n);
 
     cout << "Array after execution: " << endl;
